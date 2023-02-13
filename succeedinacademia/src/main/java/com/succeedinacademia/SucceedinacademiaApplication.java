06c46814cc5ee09f3a664da27d8545f526d142f5
Josh Anness <annessjosh@gmail.com>
2023-02-13 13:25:20 -0500

    File Structure Changes

--- succeedinacademia/src/main/java/com/succeedinacademia/SucceedinacademiaApplication.java
+++ succeedinacademia/src/main/java/com/succeedinacademia/SucceedinacademiaApplication.java
@@ -1,4 +1,4 @@
-package com.succeedinacademia.succeedinacademia;
+package com.succeedinacademia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- succeedinacademia/src/test/java/com/succeedinacademia/SucceedinacademiaApplicationTests.java
+++ succeedinacademia/src/test/java/com/succeedinacademia/SucceedinacademiaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.succeedinacademia.succeedinacademia;
+package com.succeedinacademia;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
