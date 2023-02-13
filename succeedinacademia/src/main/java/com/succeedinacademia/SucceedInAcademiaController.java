a9b1cd1180ac52f1bc5f0339a565d8e2e449cf76
Josh Anness <annessjosh@gmail.com>
2023-02-13 16:09:00 -0500

    Updated pom

--- succeedinacademia/src/main/java/com/succeedinacademia/SucceedInAcademiaController.java
+++ succeedinacademia/src/main/java/com/succeedinacademia/SucceedInAcademiaController.java
@@ -1,6 +1,8 @@
 package com.succeedinacademia;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SucceedInAcademiaController {
@@ -11,6 +13,15 @@ public class SucceedInAcademiaController {
 	 */
 	@RequestMapping(value="/start", method=RequestMethod.GET)
 	public String start() {
+		return "start";
+	}
+	
+	/**
+	 * Handle the / endpoint
+	 * @return
+	 */
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String index() {
 		return "start";
 	}
 }
