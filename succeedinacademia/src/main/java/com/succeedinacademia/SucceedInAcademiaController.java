e8db20834e10591c37e1d96dacaebae935ff8dc6
Josh Anness <annessjosh@gmail.com>
2023-02-13 15:53:02 -0500

    Controller

--- succeedinacademia/src/main/java/com/succeedinacademia/SucceedInAcademiaController.java
+++ succeedinacademia/src/main/java/com/succeedinacademia/SucceedInAcademiaController.java
@@ -4,5 +4,13 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class SucceedInAcademiaController {
-
+	
+	/**
+	 * Handle the /start endpoint
+	 * @return
+	 */
+	@RequestMapping(value="/start", method=RequestMethod.GET)
+	public String start() {
+		return "start";
+	}
 }
