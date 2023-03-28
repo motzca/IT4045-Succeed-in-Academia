303b41c27edf538761336e092074d7e8cd520ba8
Josh Anness <annessjosh@gmail.com>
2023-03-28 14:42:38 -0400

    HTML changes

--- succeedinacademia/src/main/java/com/succeedinacademia/SucceedInAcademiaController.java
+++ succeedinacademia/src/main/java/com/succeedinacademia/SucceedInAcademiaController.java
@@ -64,6 +64,27 @@ public class SucceedInAcademiaController {
 		return "start";
 	}
 	
+	@RequestMapping(value="/classes", method=RequestMethod.GET)
+	public String classes(Model model) {
+		try {
+			Iterable<ClassDTO> allClasses = classService.fetchAllClasses();
+			model.addAttribute("allClasses", allClasses);
+		} catch (Exception e) {
+			e.printStackTrace();
+			log.error("Unable to retreive classes", e);
+		}
+		
+		try {
+			Iterable<TaskDTO> allTasks = taskService.fetchAllTasks();
+			model.addAttribute("allTasks", allTasks);
+		} catch (Exception e) {
+			e.printStackTrace();
+			log.error("Unable to retreive tasks", e);
+		}
+		
+		return "classes";
+	}
+	
 	@PostMapping(value="/saveclass")
 	public ModelAndView saveClass(ClassDTO classDTO) {
 		ModelAndView modelAndView = new ModelAndView();
