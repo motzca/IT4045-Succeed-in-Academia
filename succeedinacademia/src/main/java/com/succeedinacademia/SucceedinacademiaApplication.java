93215c29f2e9644aa76dfe751e4b092bdc5ead2b
motzca <88688697+motzca@users.noreply.github.com>
2023-04-10 20:17:36 -0400

    Update SucceedinacademiaApplication.java

--- succeedinacademia/src/main/java/com/succeedinacademia/SucceedinacademiaApplication.java
+++ succeedinacademia/src/main/java/com/succeedinacademia/SucceedinacademiaApplication.java
@@ -3,6 +3,7 @@ package com.succeedinacademia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 public class SucceedinacademiaApplication {
